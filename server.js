f22d01dceb0ce1b82a73f4f76b2666cc76826193
HassanBFarhat <farhathassan@rocketmail.com>
2024-03-06 19:06:13 -0800

    Removed some non-needed files. Organized files in folders. Commented server.js file.

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+/**
+ * This file houses our Node.js server which will ultimately connect to our MySQL DB server
+ * in order to utilize the DB by fetching data to be used or posting data to different
+ * tables of the DB itself.
+ * 
+ * Authors: Hassan Farhat
+ *          Avinash Bavisetty
+ * Version: Winter 2024
+ */
+
 const express = require('express');
 const mysql = require('mysql2/promise');
-
 const bodyParser = require('body-parser');
-
-const cors = require('cors'); // Import the cors middleware
-
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-
-// Enable CORS middleware
 app.use(cors());
 
-// Create a MySQL connection pool
+// This starts the MySQL connection pool
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'admin',
@@ -21,10 +26,9 @@ const pool = mysql.createPool({
   database: 'farhat_hassan_and_bavisetty_avinash_db',
 });
 
-// Define an endpoint to fetch processors from the database
+// This endpoints GETS all the processors stored in our DB
 app.get('/api/getProcessors', async (req, res) => {
   try {
-    // Fetch processors from the database
     const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [1]);
     res.json(rows);
   } catch (error) {
@@ -33,111 +37,107 @@ app.get('/api/getProcessors', async (req, res) => {
   }
 });
 
+// This endpoints GETS all the Graphics Cards stored in our DB
 app.get('/api/getGPUs', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [2]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getMemory', async (req, res) => {
+// This endpoints GETS all the Memory stored in our DB
+app.get('/api/getMemory', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [3]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getStorage', async (req, res) => {
+// This endpoints GETS all the Storage stored in our DB
+app.get('/api/getStorage', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [4]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getMOBOs', async (req, res) => {
+// This endpoints GETS all the Motherboards stored in our DB
+app.get('/api/getMOBOs', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [5]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getPSUs', async (req, res) => {
+// This endpoints GETS all the Power Supplies stored in our DB
+app.get('/api/getPSUs', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [6]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getCases', async (req, res) => {
+// This endpoints GETS all the Cases stored in our DB
+app.get('/api/getCases', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [7]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getCooling', async (req, res) => {
+// This endpoints GETS all the Cooling Items stored in our DB
+app.get('/api/getCooling', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [8]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
-  app.get('/api/getPeripherals', async (req, res) => {
+// This endpoints GETS all the Peripherals stored in our DB
+app.get('/api/getPeripherals', async (req, res) => {
     try {
-      // Fetch processors from the database
       const [rows, fields] = await pool.query('SELECT * FROM Product WHERE CategoryID = ?', [9]);
       res.json(rows);
     } catch (error) {
       console.error('Error fetching processors:', error);
       res.status(500).json({ error: 'Failed to fetch processors' });
     }
-  });
+});
 
+// This endpoints POSTS the new item that was added to our store's DB
 app.post('/api/addNewItem', async (req, res) => {
   try {
-    // Check if all required properties are present in the request body
     const { CategoryID, Brand, ProductName, UnitPrice } = req.body;
     if (!CategoryID || !Brand || !ProductName || !UnitPrice) {
       return res.status(400).json({ error: 'Missing required fields' });
     }
-
-    // Insert the new item into the database
     const connection = await pool.getConnection();
     await connection.execute(
         `INSERT INTO Product (CategoryID, Brand, ProductName, UnitPrice) VALUES (?, ?, ?, ?)`,
         [CategoryID, Brand, ProductName, UnitPrice]
     );
     connection.release();
-
-    // Respond with success message
     return res.status(200).json({ message: 'Item added successfully' });
   } catch (error) {
     console.error('Error adding item:', error);
@@ -145,22 +145,19 @@ app.post('/api/addNewItem', async (req, res) => {
   }
 });
 
+// This endpoints POSTS the transactions made from each item a user buys from our DB
 app.post('/api/transactions', async (req, res) => {
   try {
     const { ProductID, Transaction, Quantity, TransactionDate } = req.body;
     if (!ProductID || !Transaction || !Quantity || !TransactionDate) {
       return res.status(400).json({ error: 'Missing required fields' });
     }
-
-    // Perform the insert operation in the database
     const connection = await pool.getConnection();
     await connection.execute(
       'INSERT INTO Transaction (ProductID, Transaction, Quantity, TransactionDate) VALUES (?, ?, ?, ?)',
       [ProductID, Transaction, Quantity, TransactionDate]
     );
     connection.release();
-
-    // Respond with success message
     res.status(200).json({ message: 'Transaction added successfully' });
   } catch (error) {
     console.error('Error adding transaction:', error);
@@ -168,7 +165,7 @@ app.post('/api/transactions', async (req, res) => {
   }
 });
 
-// Start the server
+// This starts the node.js server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
